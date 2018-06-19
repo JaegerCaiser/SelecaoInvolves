f53ac7cc08de31afd293d42634be780a8db77467
Matheus Henrique Caiser Barrozo <matheus.kaiser@gmail.com>
2018-06-19 01:25:17 -0300

    Refresh na tela de alertas

--- app-angular/src/app/alertas/alertas.component.ts
+++ app-angular/src/app/alertas/alertas.component.ts
@@ -1,17 +1,29 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {AlertasService} from "./alertas.service";
 import {AlertaModel} from "./alertaModel";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
+import {Subscription} from "rxjs/internal/Subscription";
 
 @Component({
     selector: 'app-alertas',
     templateUrl: './alertas.component.html'
 })
-export class AlertasComponent implements OnInit {
+export class AlertasComponent implements OnInit, OnDestroy {
 
     alertas: AlertaModel[] = []
+    private mySubscription: Subscription;
 
     constructor(private alertasService: AlertasService, private router: Router) {
+        this.router.routeReuseStrategy.shouldReuseRoute = function () {
+            return false;
+        };
+
+        this.mySubscription = this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                // Trick the Router into believing it's last link wasn't previously loaded
+                this.router.navigated = false;
+            }
+        });
     }
 
     ngOnInit(): void {
@@ -22,8 +34,13 @@ export class AlertasComponent implements OnInit {
     processar(): void {
         this.alertasService.processar()
             .subscribe((data) => {
-                this.router.navigate(['/'])
+                this.router.navigate(['/alertas'])
             })
+    }
+
+    ngOnDestroy(): void {
+        if (this.mySubscription)
+            this.mySubscription.unsubscribe();
     }
 
 }
